680f1ef9f0d271a978f834f8c0eb4992f9328166
tGulevich <t.ananich@gmail.com>
2018-10-28 18:14:34 +0300

    Loading map

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 "use strict";
 (function () {
     var MAP_SRC = 'img/map.png';
-    var TIME_INTERVAL = 2500;
+    var TIME_INTERVAL = 25;
     var ZONE_ONE_COLOR = [161,34,234];
     var ZONE_TWO_COLOR = [0,129,241];
     var ZONE_THREE_COLOR = [255,41,83];
@@ -11,18 +11,20 @@
     var ZONE_SEVEN_COLOR = [0,202,148];
     var ZONE_EIGHT_COLOR = [0,185,247];
 
+    
+
     var mapImage = document.querySelector('#mapImage');
     var value = document.querySelector('#value');
-    var point = document.querySelector('.point');
-    var line = document.querySelector('.line');
+    //var point = document.querySelector('.point');
+    //var line = document.querySelector('.line');
     var points = document.querySelectorAll('.point');
-    var lines = document.querySelectorAll('.line');
+    var lines = document.querySelectorAll('.line--colored');
 
-    value.textContent = '100';
+    //value.textContent = '0';
 
 
     // Change color of zone
-    var changeColorPixel = function (data, newColor, oldColor1, oldColor2, oldColor3, oldColor4, oldColor5, oldColor6, oldColor7, oldColor8)
+    var changeColorOfZone = function (data, newColor, oldColor1, oldColor2, oldColor3, oldColor4, oldColor5, oldColor6, oldColor7, oldColor8)
     {
         var img = document.createElement('img');
         img.src = data;
@@ -31,8 +33,6 @@
             mapImage.src = draw(this);
         };
         
-        
-
         function draw(img) {
 
             var cnv, ctx, imgData;
@@ -42,15 +42,11 @@
             cnv.height = img.height;
         
             ctx = cnv.getContext('2d');
-            
             ctx.drawImage(img, 0, 0);
-        
             imgData = ctx.getImageData(0, 0, cnv.width, cnv.height);
             data = imgData.data;
         
-            
             for(var x = 0, len = data.length; x < len; x += 4) {   
-                
 
                 if((data[x] == oldColor1[0]) &&
                     (data[x + 1] >= oldColor1[1]-15 && data[x + 1] <= oldColor1[1]+15) &&
@@ -59,7 +55,6 @@
                     data[x] = newColor[0];
                     data[x + 1] = newColor[1];
                     data[x + 2] = newColor[2];
-
                 }
 
                 if((data[x] == oldColor2[0]) &&
@@ -69,7 +64,6 @@
                     data[x] = newColor[0];
                     data[x + 1] = newColor[1];
                     data[x + 2] = newColor[2];
-
                 }
 
                 if((data[x] == oldColor3[0]) &&
@@ -79,7 +73,6 @@
                     data[x] = newColor[0];
                     data[x + 1] = newColor[1];
                     data[x + 2] = newColor[2];
-
                 }
 
                 if((data[x] == oldColor4[0]) &&
@@ -89,7 +82,6 @@
                     data[x] = newColor[0];
                     data[x + 1] = newColor[1];
                     data[x + 2] = newColor[2];
-
                 }
 
                 if((data[x] == oldColor5[0]) &&
@@ -99,7 +91,6 @@
                     data[x] = newColor[0];
                     data[x + 1] = newColor[1];
                     data[x + 2] = newColor[2];
-
                 }
 
                 if((data[x] == oldColor6[0]) &&
@@ -109,7 +100,6 @@
                     data[x] = newColor[0];
                     data[x + 1] = newColor[1];
                     data[x + 2] = newColor[2];
-
                 }
 
                 if((data[x] == oldColor7[0]) &&
@@ -119,7 +109,6 @@
                     data[x] = newColor[0];
                     data[x + 1] = newColor[1];
                     data[x + 2] = newColor[2];
-
                 }
 
                 if((data[x] == oldColor8[0]) &&
@@ -129,7 +118,6 @@
                     data[x] = newColor[0];
                     data[x + 1] = newColor[1];
                     data[x + 2] = newColor[2];
-
                 }
 
             }
@@ -142,39 +130,39 @@
 
 
     // Change color of all map
-    var changeColorImage = function (data, newColor) {
+    var changeColorOfAllMap = function (data, newColor) {
         var img, cnv, ctx, imgData;
     
         img = document.createElement('img');
         img.src = data;
-    
-        cnv = document.createElement('canvas');
-        cnv.width = img.width;
-        cnv.height = img.height;
-    
-        ctx = cnv.getContext('2d');
+
+        img.onload = function() {
+            mapImage.src = draw(this);
+        };
         
-        ctx.drawImage(img, 0, 0);
-    
-        imgData = ctx.getImageData(0, 0, cnv.width, cnv.height)
+        function draw(img) {
+            cnv = document.createElement('canvas');
+            cnv.width = img.width;
+            cnv.height = img.height;
         
-        data = imgData.data;
-    
+            ctx = cnv.getContext('2d');
+            ctx.drawImage(img, 0, 0);
+            imgData = ctx.getImageData(0, 0, cnv.width, cnv.height)
+            data = imgData.data;
         
-        for(var x = 0, len = data.length; x < len; x += 4) {   
-            
-            if(data[x + 3] > 0) {   
-
-                data[x] = newColor[0];
-                data[x + 1] = newColor[1];
-                data[x + 2] = newColor[2];
-
-            } 
-
-        }
-
-        ctx.putImageData(imgData, 0, 0);
-        return cnv.toDataURL();
+            for(var x = 0, len = data.length; x < len; x += 4) {   
+                
+                if(data[x + 3] > 0) {   
+                    data[x] = newColor[0];
+                    data[x + 1] = newColor[1];
+                    data[x + 2] = newColor[2];
+                } 
+    
+            }
+    
+            ctx.putImageData(imgData, 0, 0);
+            return cnv.toDataURL();
+        };
         
     }
 
@@ -182,33 +170,51 @@
     // Inactive progress bar
 
     var resetProgress = function () {
+        value.textContent = '0';
+
         for (var i = 0; i < points.length; i++) {
             points[i].classList.add('point--inactive');
         }
-        mapImage.src = changeColorImage(MAP_SRC, [173,173,173]);
-        
-    }
 
+        for (var j = 0; j < lines.length; j++) {
+            lines[j].style.width = 0;
+        }
+        changeColorOfAllMap(MAP_SRC, [173,173,173]);
+        
+    };
     resetProgress();
 
+    var start = function () {
+
+    };
+
+    var stop = function () {
+
+    };
     
-
-
+    var getCurrentPointValue = function () {
+        console.log(value.textContent)
+        return value.textContent;
+    };
+    
+    
     var loadMap = function() {
 
         var turnOnEighthZone = function () {
             function move() {
                 var yyy = document.querySelector('.line-8.line--colored');
                 var width = 0;
-                var id = setInterval(frame, 15);
+                var id = setInterval(frame, TIME_INTERVAL);
                 function frame() {
                     if (width >= 100) {
                         clearInterval(id);
                         callback();
+                        setTimeout(function() {
+                            loadMap();
+                        },1000);
                     } else {
                         width++; 
-                        yyy.style.width = width + '%'; 
-                        value.textContent = +value.textContent + 1;
+                        yyy.style.width = width + '%';
                     }
                 }
     
@@ -219,9 +225,9 @@
             function callback() {
                 var zoneTwo = document.querySelector('.point-8');
                 zoneTwo.classList.remove('point--inactive');
-            
+                value.textContent = +value.textContent + 100; 
+
                 mapImage.src = MAP_SRC;
-            
             };
     
         };
@@ -231,7 +237,7 @@
             function move() {
                 var yyy = document.querySelector('.line-7.line--colored');
                 var width = 0;
-                var id = setInterval(frame, 15);
+                var id = setInterval(frame, TIME_INTERVAL);
                 function frame() {
                     if (width >= 100) {
                         clearInterval(id);
@@ -240,7 +246,6 @@
                     } else {
                         width++; 
                         yyy.style.width = width + '%';
-                        value.textContent = +value.textContent + 1;
                     }
                 }
     
@@ -251,10 +256,11 @@
             function callback() {
                 var zoneTwo = document.querySelector('.point-7');
                 zoneTwo.classList.remove('point--inactive');
-            
-                changeColorPixel(MAP_SRC, [173,173,173],
+
+                changeColorOfZone(MAP_SRC, [173,173,173],
                     ZONE_EIGHT_COLOR, 0, 0, 0, 0, 0, 0, 0);
-            
+
+                value.textContent = +value.textContent + 100; 
             };
     
         };
@@ -264,7 +270,7 @@
             function move() {
                 var yyy = document.querySelector('.line-6.line--colored');
                 var width = 0;
-                var id = setInterval(frame, 15);
+                var id = setInterval(frame, TIME_INTERVAL);
                 function frame() {
                     if (width >= 100) {
                         clearInterval(id);
@@ -273,7 +279,6 @@
                     } else {
                         width++; 
                         yyy.style.width = width + '%';
-                        value.textContent = +value.textContent + 1;
                     }
                 }
     
@@ -285,9 +290,10 @@
                 var zoneTwo = document.querySelector('.point-6');
                 zoneTwo.classList.remove('point--inactive');
             
-                changeColorPixel(MAP_SRC, [173,173,173],
+                changeColorOfZone(MAP_SRC, [173,173,173],
                     ZONE_SEVEN_COLOR, ZONE_EIGHT_COLOR, 0, 0, 0, 0, 0, 0);
             
+                value.textContent = +value.textContent + 100;                     
             };
     
         };
@@ -297,7 +303,7 @@
             function move() {
                 var yyy = document.querySelector('.line-5.line--colored');
                 var width = 0;
-                var id = setInterval(frame, 15);
+                var id = setInterval(frame, TIME_INTERVAL);
                 function frame() {
                     if (width >= 100) {
                         clearInterval(id);
@@ -306,7 +312,6 @@
                     } else {
                         width++; 
                         yyy.style.width = width + '%';
-                        value.textContent = +value.textContent + 1;
                     }
                 }
     
@@ -318,9 +323,10 @@
                 var zoneTwo = document.querySelector('.point-5');
                 zoneTwo.classList.remove('point--inactive');
             
-                changeColorPixel(MAP_SRC, [173,173,173],
+                changeColorOfZone(MAP_SRC, [173,173,173],
                     ZONE_SIX_COLOR, ZONE_SEVEN_COLOR, ZONE_EIGHT_COLOR, 0, 0, 0, 0, 0);
-            
+                    
+                value.textContent = +value.textContent + 100; 
             };
     
         };
@@ -330,7 +336,7 @@
             function move() {
                 var yyy = document.querySelector('.line-4.line--colored');
                 var width = 0;
-                var id = setInterval(frame, 15);
+                var id = setInterval(frame, TIME_INTERVAL);
                 function frame() {
                     if (width >= 100) {
                         clearInterval(id);
@@ -339,7 +345,6 @@
                     } else {
                         width++; 
                         yyy.style.width = width + '%';
-                        value.textContent = +value.textContent + 1;
                     }
                 }
     
@@ -351,9 +356,10 @@
                 var zoneTwo = document.querySelector('.point-4');
                 zoneTwo.classList.remove('point--inactive');
             
-                changeColorPixel(MAP_SRC, [173,173,173],
+                changeColorOfZone(MAP_SRC, [173,173,173],
                     ZONE_FIVE_COLOR, ZONE_SIX_COLOR, ZONE_SEVEN_COLOR, ZONE_EIGHT_COLOR, 0, 0, 0, 0);
-            
+                
+                value.textContent = +value.textContent + 100; 
             };
     
         };
@@ -363,7 +369,7 @@
             function move() {
                 var yyy = document.querySelector('.line-3.line--colored');
                 var width = 0;
-                var id = setInterval(frame, 15);
+                var id = setInterval(frame, TIME_INTERVAL);
                 function frame() {
                     if (width >= 100) {
                         clearInterval(id);
@@ -372,7 +378,7 @@
                     } else {
                         width++; 
                         yyy.style.width = width + '%';
-                        value.textContent = +value.textContent + 1; 
+                        
                     }
                 }
     
@@ -384,9 +390,10 @@
                 var zoneTwo = document.querySelector('.point-3');
                 zoneTwo.classList.remove('point--inactive');
             
-                changeColorPixel(MAP_SRC, [173,173,173],
+                changeColorOfZone(MAP_SRC, [173,173,173],
                     ZONE_FOUR_COLOR, ZONE_FIVE_COLOR, ZONE_SIX_COLOR, ZONE_SEVEN_COLOR, ZONE_EIGHT_COLOR, 0, 0, 0);
-            
+                
+                value.textContent = +value.textContent + 100; 
             };
     
         };
@@ -396,16 +403,17 @@
             function move() {
                 var yyy = document.querySelector('.line-2.line--colored');
                 var width = 0;
-                var id = setInterval(frame, 15);
+                var id = setInterval(frame, TIME_INTERVAL);
                 function frame() {
                     if (width >= 100) {
                         clearInterval(id);
                         callback();
                         turnOnThirdZone();
+                        
                     } else {
                         width++; 
                         yyy.style.width = width + '%';
-                        value.textContent = +value.textContent + 1;
+                        
                     }
                 }
     
@@ -417,27 +425,37 @@
                 var zoneTwo = document.querySelector('.point-2');
                 zoneTwo.classList.remove('point--inactive');
             
-                changeColorPixel(MAP_SRC, [173,173,173],
+                changeColorOfZone(MAP_SRC, [173,173,173],
                     ZONE_THREE_COLOR, ZONE_FOUR_COLOR, ZONE_FIVE_COLOR, ZONE_SIX_COLOR, ZONE_SEVEN_COLOR, ZONE_EIGHT_COLOR, 0, 0);
             
+                value.textContent = +value.textContent + 100;
             };
         };
 
         var turnOnFirstZone = function () {
-
-            
             var zoneOne = document.querySelector('.point-1');
             zoneOne.classList.remove('point--inactive');
             
-            changeColorPixel(MAP_SRC, [173,173,173],
+            changeColorOfZone(MAP_SRC, [173,173,173],
                 ZONE_TWO_COLOR, ZONE_THREE_COLOR, ZONE_FOUR_COLOR, ZONE_FIVE_COLOR, ZONE_SIX_COLOR, ZONE_SEVEN_COLOR, ZONE_EIGHT_COLOR, 0);
             
+            value.textContent = 100; 
             turnOnSecondZone();
-        }();
+        };
 
-    }();
+        setTimeout(function() {
+            turnOnFirstZone();
+        },1000);
+    
+        resetProgress();
 
+    };
+
+    loadMap();
+    
     
 
+
+    
 
 })();
